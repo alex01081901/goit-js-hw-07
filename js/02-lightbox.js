a0bd40389d9b011e21ac2b6560e99151b0d50b4d
alex01081901 <114284497+alex01081901@users.noreply.github.com>
2023-03-18 18:06:13 +0200

    hjk

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -2,3 +2,23 @@ import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
 console.log(galleryItems);
+const galleryEl = document.querySelector(".gallery");
+
+const galleryItem = galleryItems
+    .map(({ preview, original, description }) => {
+        return `<a class="gallery__item" href="${original}">
+            <img
+              class="gallery__image "
+              src="${preview}"
+              data-source="${original}"
+              alt="${description}"
+              />
+              </a>`;
+    })
+    .join("");
+galleryEl.insertAdjacentHTML("beforeend", galleryItem);
+
+new SimpleLightbox(".gallery a", {
+    captionsData: "alt",
+    captionDelay: 250,
+});
